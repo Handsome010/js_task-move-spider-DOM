52e94e0ef4f08c7398a244c6a170b99732adec97
Danyliuk Yaroslav <unityismylove@gmail.com>
2026-07-14 19:53:26 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,19 @@
 'use strict';
 
-document.addEventListener('click', (e) => {
-  // write code here
+const wall = document.querySelector('.wall');
+const spider = document.querySelector('.spider');
+
+wall.addEventListener('click', (e) => {
+  // const rect = wall.getBoundingClientRect();
+  const x = e.clientX - spider.offsetWidth / 2;
+  const y = e.clientY;
+
+  // if (x < rect.left + spider.offsetWidth / 2 - ) {
+  //   console.log('left');
+
+  // }
+
+  spider.style.position = 'fixed';
+  spider.style.left = `${x}px`;
+  spider.style.top = `${y}px`;
 });
