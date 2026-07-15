91bef95b15e65eea08020346cd43fe6210ace312
Danyliuk Yaroslav <unityismylove@gmail.com>
2026-07-15 21:04:11 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -7,9 +7,10 @@ wall.addEventListener('click', (e) => {
   // const rect = wall.getBoundingClientRect();
   const x = e.clientX - spider.offsetWidth / 2;
   const y = e.clientY;
+  // const borderWidth = getComputedStyle(wall).borderWidth;
 
-  // if (x < rect.left + spider.offsetWidth / 2 - ) {
-  //   console.log('left');
+  // if (x < rect.left + spider.offsetWidth / 2 - borderWidth) {
+
 
   // }
 
